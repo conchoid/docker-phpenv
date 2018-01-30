c73b13653142541a4fd483471c0c260553402b57
Takahito Natsui <Takahito.x.Natsui@sony.com>
2018-01-30 15:22:52 +0900

    Update php build dependencies

--- 5.6-alpine3.6/install-builddeps.sh
+++ 5.6-alpine3.6/install-builddeps.sh
@@ -4,6 +4,7 @@ set -eux
 
 # TODO: Fix package version
 apk add --no-cache \
+    apache2-dev \
     autoconf \
     bash \
     bzip2-dev \
@@ -31,8 +32,8 @@ apk add --no-cache \
     make \
     mercurial \
     openssl \
-    openssl-dev \
     openssh \
+    patch \
     php5-dev \
     pkgconf \
     re2c \
@@ -51,3 +52,17 @@ rm lemon.c
 
 # buffio.h is deprecated but needed for building php prior to 7.2.
 curl -o /usr/include/buffio.h https://raw.githubusercontent.com/htacg/tidy-html5/5.6.0/include/buffio.h
+
+# bison 2.x is required for building php5.x
+WORK_DIR="/tmp/bison/"
+BISON_VERSION="2.6.4"
+mkdir -p $WORK_DIR/downloads $WORK_DIR/src
+wget http://ftp.gnu.org/gnu/bison/bison-$BISON_VERSION.tar.gz -P $WORK_DIR/downloads/
+tar -zxf $WORK_DIR/downloads/bison-$BISON_VERSION.tar.gz -C $WORK_DIR/src
+(
+  cd $WORK_DIR/src/bison-$BISON_VERSION/
+  ./configure --prefix=/usr/local/lib/bison-$BISON_VERSION
+  make && make install
+  ln -s /usr/local/lib/bison-$BISON_VERSION/bin/bison /usr/local/bin/bison
+)
+rm -rf $WORK_DIR
