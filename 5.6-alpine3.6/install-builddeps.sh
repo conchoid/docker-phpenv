ea69d02eb5c4843f5b9222cdcdab026fb31e9ea4
ks888sk <kishin.yagami@jp.sony.com>
2018-03-20 14:13:45 +0900

    Update install-builddeps.sh

--- 5.6-alpine3.6/install-builddeps.sh
+++ 5.6-alpine3.6/install-builddeps.sh
@@ -8,7 +8,7 @@ apk add --no-cache \
     bzip2-dev="1.0.6-r5" \
     ca-certificates="20161130-r2" \
     coreutils="8.27-r0" \
-    curl="7.58.0-r1" \
+    curl="7.59.0-r0" \
     curl-dev="7.59.0-r0" \
     dpkg="1.18.23-r2" \
     dpkg-dev="1.18.23-r2" \
