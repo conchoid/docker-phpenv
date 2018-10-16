6cbbfd500afa6964d88ec5a7e9fdb0e94d8cba6b
Yoshito Ishii <yoshito.ishii@upstairs.jp>
2018-10-16 17:41:00 +0900

    Preset locale to en_US.UTF-8

--- 5.6-stretch/install-builddeps.sh
+++ 5.6-stretch/install-builddeps.sh
@@ -3,10 +3,10 @@
 set -eux
 
 apt-get -y install apt-transport-https="1.4.8" \
-&& apt-get -y install --allow-downgrades openssl="1.0.2j-1+0~20161013114109.8+stretch~1.gbpac057a" \
-  libssl1.0.2="1.0.2j-1+0~20161013114109.8+stretch~1.gbpac057a" \
-  libssl-dev="1.0.2j-1+0~20161013114109.8+stretch~1.gbpac057a" \
-  libssl-doc="1.0.2j-1+0~20161013114109.8+stretch~1.gbpac057a" \
+&& apt-get -y install --allow-downgrades openssl="1.0.1t-1+deb8u8" \
+  libssl1.0.2="1.0.2l-2+deb9u3" \
+  libssl-dev="1.0.1t-1+deb8u8" \
+  libssl-doc="1.0.1t-1+deb8u8" \
 && apt-get -y install  \
   autoconf="2.69-10" \
   ca-certificates="20161130+nmu1+deb9u1" \
@@ -17,10 +17,10 @@ apt-get -y install apt-transport-https="1.4.8" \
   file="1:5.30-1+deb9u2" \
   g++="4:6.3.0-4" \
   gcc="4:6.3.0-4" \
-  git="1:2.11.0-3+deb9u3" \
+  git \
   gnupg="2.1.18-8~deb9u2" \
   libbz2-dev="1.0.6-8.1" \
-  libcurl4-gnutls-dev="7.52.1-5+deb9u6" \
+  libcurl4-gnutls-dev="7.52.1-5+deb9u7" \
   libedit-dev="3.1-20160903-3" \
   libevent-dev="2.0.21-stable-3" \
   libghc-readline-dev="1.0.3.0-7" \
@@ -34,7 +34,7 @@ apt-get -y install apt-transport-https="1.4.8" \
   make="4.1-9.1" \
   mercurial="4.0-1+deb9u1" \
   patch="2.7.5-1+deb9u1" \
-  php5.6-dev="5.6.37-1+0~20180725093819.2+stretch~1.gbp606419" \
+  php5.6-dev \
   pkgconf="0.9.12-6" \
   python-dev="2.7.13-2" \
   python="2.7.13-2" \
@@ -43,6 +43,7 @@ apt-get -y install apt-transport-https="1.4.8" \
   subversion="1.9.5-1+deb9u2" \
   tar="1.29b-1.1" \
   tidy="1:5.2.0-2+0~20180714172546.1+stretch~1.gbpc7c60a" \
+  libcurl3="7.52.1-5+deb9u6" \
   wget="1.18-5+deb9u2"
 apt-get clean
 rm -rf /var/lib/apt/lists/*
