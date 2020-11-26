c53788680febf14ce49dcc6ec7d06b231154b5bd
ups-oikawa <masato.oikawa@upstairs.jp>
2020-11-26 14:32:56 +0900

    Fixed to preinstall with docker-phpenv, to eliminate conchoid/docker-phpenv-builtins. (#6)

--- 7.4-stretch/install-builddeps.sh
+++ 7.4-stretch/install-builddeps.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 set -eux
-
+# php install build deps
 apt-get update && apt-get -y --allow-downgrades install  \
   autoconf \
   coreutils \
@@ -9,7 +9,6 @@ apt-get update && apt-get -y --allow-downgrades install  \
   dpkg-dev \
   file \
   g++ \
-  gcc \
   gnupg \
   libbz2-dev \
   libcurl3 \
@@ -24,12 +23,10 @@ apt-get update && apt-get -y --allow-downgrades install  \
   libreadline-dev \
   libsqlite3-dev \
   libtidy-dev \
-  libxml2="2.9.4+dfsg1-2.2+deb9u2" \
   libxml2-dev="2.9.4+dfsg1-2.2+deb9u2" \
+  libxml2="2.9.4+dfsg1-2.2+deb9u2" \
   libxslt1-dev \
   libzip-dev \
-  m4 \
-  make \
   mercurial \
   patch \
   pkgconf \
@@ -37,8 +34,7 @@ apt-get update && apt-get -y --allow-downgrades install  \
   python-dev \
   re2c \
   sqlite3 \
-  tar \
-  tidy 
+  tidy
 
 # openssl1.0.x and related libssl packages are required for installing php5.x by phpenv.
 echo "deb http://ftp.br.debian.org/debian/ jessie main" > /etc/apt/sources.list.d/jessie.list 
@@ -50,21 +46,3 @@ rm -f /etc/apt/sources.list.d/jessie.list
 
 apt-get clean
 rm -rf /var/lib/apt/lists/*
-
-# buffio.h is deprecated but needed for building php prior to 7.2.
-curl -o /usr/include/buffio.h https://raw.githubusercontent.com/htacg/tidy-html5/5.6.0/include/buffio.h
-
-# bison 2.x is required for building php5.x
-WORK_DIR="/tmp/bison/"
-BISON_VERSION="2.6.4"
-mkdir -p ${WORK_DIR}/downloads ${WORK_DIR}/src
-wget http://ftp.gnu.org/gnu/bison/bison-${BISON_VERSION}.tar.gz -P ${WORK_DIR}/downloads/
-tar -zxf ${WORK_DIR}/downloads/bison-${BISON_VERSION}.tar.gz -C ${WORK_DIR}/src
-(
-  cd ${WORK_DIR}/src/bison-${BISON_VERSION}/
-  ./configure --prefix=/usr/local/lib/bison-${BISON_VERSION}
-  make && make install
-  ln -s /usr/local/lib/bison-${BISON_VERSION}/bin/bison /usr/local/bin/bison
-)
-rm -rf ${WORK_DIR}
-ln -s /usr/include/x86_64-linux-gnu/curl /usr/include/curl
